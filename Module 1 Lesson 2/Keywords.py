3e116eca98738180f6c6e582453263782b0fdfbb
Kush-Shelat <kushshelat@gmail.com>
2026-08-25 21:45:03 -0400

    Add files via upload

---
+++ Module 1 Lesson 2/Keywords.py
@@ -0,0 +1,5 @@
+import keyword
+
+# printing all the python keywords
+print("python keywords are...\n")
+print(keyword.kwlist)
---
+++ Module 1 Lesson 2/Understanding Variables.py
@@ -0,0 +1,13 @@
+x = "Kush"
+y = 7
+print (x)
+print (y)
+
+
+codingal = "codingal"
+print(codingal)
+
+# The input of the user
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
