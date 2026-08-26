350bf823ffb565872ea3e690188ed9ca95b3b552
Kush-Shelat <kushshelat@gmail.com>
2026-08-25 21:40:49 -0400

    Add files via upload

---
+++ Python Course/Lesson 2/LESSON 2 PROJECT.py
@@ -0,0 +1,38 @@
+# Giving variables their value
+x = "Welcome to Kush's daily Tracker"
+y = "This is the daily routine of Kush shelat"
+
+# Printing the Variables
+print (x )
+print (y)
+
+
+# The input of the user
+name = input("Would you like to see the daily tracker?")
+
+# Making aliright statement
+z = ("Alright then! Here is the daily tracker: \n")
+
+# Printing aliright statement
+print (z)
+
+
+# Title
+print("My Daily Routine \n")
+
+# Step 1: Waking up at 7 AM
+print("I start by waking up at 7 AM. \n")
+
+# Step 2: Going to school at 8 AM
+print("I then go to school at exactly 8 AM. \n")
+
+# Step 3: Playing with friends after school (in evening)
+print("I play with my friends in the evening. \n")
+
+# Step 4: 2 hour study tme
+print("I study for 2 hours at night. \n")
+
+
+
+# My calculations for total study time.
+print("Total study hours in a week:", 2 * 7)
