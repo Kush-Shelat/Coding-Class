50c83797a6bf21637dc6e12f756f91b7352d24f4
Kush-Shelat <kushshelat@gmail.com>
2026-09-04 20:56:27 -0400

    Add files via upload

---
+++ Module 1 Lesson 4/string_operation.py
@@ -0,0 +1,6 @@
+text = str(input("enter a string: "))
+revtext = text[::-1]
+text = revtext
+print (text)
+
+            
---
+++ Module 1 Lesson 4/typecasting.py
@@ -0,0 +1,25 @@
+# Assigning Different Variables
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+# Printing Different Variables and their Data Type
+print ("Name :", name)
+print("Data Type of Name is", type(name))
+
+print ("Age: ", age)
+print ("Data type of age is", type(age))
+
+print("is_student: ", is_student)
+print ("Data type of is_student is", type(is_student))
+
+print ("weight", weight)
+print ("Data type of weight is", type(weight))
+
+print("\n After Casting....")
+age = str(age)
+print(age)
+print ("data type of age is", type (age))
+weight = int (weight)
+print ("Data type of weight is", type (weight))
