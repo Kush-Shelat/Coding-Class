3dbc613615be5e936d69446bacf52d4eca84ed92
Kush-Shelat <kushshelat@gmail.com>
2026-09-15 21:44:50 -0400

    Add files via upload

---
+++ Module 3 Lesson 2/Reverse a string.py
@@ -0,0 +1,8 @@
+string = input("please enter your own string : ")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print ("\nThe orignal String = ", string)
+print ("the reversed string = ", string2)
---
+++ Module 3 Lesson 2/Sum of whole numbers.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the number whose sum you want to fin: "))
+sum = 0
+
+for i in range(1, n+1):
+    sum = sum+1
+    print("/nsum=", sum)
+    
