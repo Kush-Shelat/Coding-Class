680d6840d13d75b6fed45dac440cf56101523ced
Kush-Shelat <kushshelat@gmail.com>
2026-09-25 19:52:07 -0400

    Add files via upload

---
+++ Module 3 Lesson 4/Char occur.py
@@ -0,0 +1,14 @@
+string = input("please enter your own word : ")
+
+char = input("please enter your own character : ")
+i=0
+count=0
+
+while(i < len(string)):
+
+    if(string[i] == char):
+        count=count + 1
+    i = i + 1
+    
+print("the toal number of time", char, "has occured =", count)
+
